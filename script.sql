d59c39d44cae5f9de27e5f895846cc72fe71da78
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-11-19 11:15:55 -0500

    did some visualization & feature eningeering; next will be exploding genre, studio, and producer columns & setting up distinct sql tables for continued analysis

--- script.sql
+++ script.sql
@@ -17,11 +17,12 @@ create table if NOT EXISTS anime (
     rank FLOAT,
     rating VARCHAR(500),
     studios VARCHAR(500),
-    producers VARCHAR(5000)
+    producers VARCHAR(5000),
+    episodes_category VARCHAR(500)
 )
 
-/copy anime(animeID, id, name, genres, type, episodes, status, airedFrom, airedTo, duration, score, ratingUsers, rank, rating, studios, producers)
-from '/tmp/popularAnime.csv'
+/copy anime(animeID, id, name, genres, type, episodes, status, airedFrom, airedTo, duration, score, ratingUsers, rank, rating, studios, producers, episodes_category)
+from '/tmp/popularanime.csv'
 DELIMITER ','
 csv header;
 
@@ -34,4 +35,32 @@ order by rank asc;
 select * from anime 
 where rating like '%violence%'
 
-select DISTINCT(producers) from anime
+select DISTINCT(type), count(animeID) as type_count from anime
+group by type
+order by type_count desc;
+
+select animeID, id, name, type, episodes, status from anime
+where not type = 'TV' and name not like '%Season%'
+
+select animeID, name, episodes, episodes_category from anime
+where episodes_category = '1 - 12 Episodes'
+order by episodes desc;
+-- top episode count is 440 --
+
+select animeID, name, episodes, episodes_category from anime
+where episodes_category = '13 - 25 Episodes'
+order by episodes desc;
+-- from 475 to 726 (only 4 animes) --
+
+select animeID, name, episodes, episodes_category from anime
+where episodes_category = '100+ Episodes'
+order by episodes desc;
+
+select animeID, name, episodes from anime
+where episodes > 0 and episodes < 50
+order by episodes desc;
+
+select count(episodes) as episodeCount, type from anime
+where episodes > 0 and episodes < 50
+group by type
+order by episodeCount desc;
